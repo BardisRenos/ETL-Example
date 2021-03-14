018dbcb92b7c007c98464df976898b8d74f13b25
Renos Bardis <renosbardis@gmail.com>
2021-03-14 19:33:01 +0100

    Creating a method to convert the csv to database

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from util import read_data
+from util import read_data, data_to_db_format
 
 if __name__ == '__main__':
     file_path = "/home/renos/Downloads/archive/aug_train.csv"
     res = read_data(file_name=file_path)
     print(res.head(5))
-
--- util.py
+++ util.py
@@ -1,8 +1,20 @@
 import pandas as pd
+import csv_to_sqlite
+import sqlite3
 
 
+# Read the csv file
 def read_data(file_name):
     with open(file=file_name) as csv_file:
         csv_reader = pd.read_csv(csv_file)
 
     return csv_reader
+
+
+# Convert the a csv file into database format
+def data_to_db_format(file_name):
+    options = csv_to_sqlite.CsvOptions(typing_style="full", encoding="windows-1250")
+    input_files = [file_name]
+
+    return csv_to_sqlite.write_csv(input_files, "output.db", options)
+
